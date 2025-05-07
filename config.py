955c2540f6cbda850755115ebfa8c291fd249ca4
RRR-dot43 <ruhilrohit10@gmail.com>
2025-05-07 12:58:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-api_id = "24473318"
-api_hash = "e7dd0576c5ac0ff8f90971d6bb04c8f5"
-bot_token = "7890037285:AAFdjqWspAuiieax00EtqDm7C4KsrDDzk0k"
+api_id = "25967358"
+api_hash = "10a5a31171dfdc323efdbcf84a8fb016"
+bot_token = "7844118495:AAEPSmKwyHhgMUNNpIORPWGfatMfXf4Qi5A"
 log_channel = -1002601604234
 force_sub_channel = -1002528475274
