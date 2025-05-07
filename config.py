db5eadb064cebd33ff5891f79a5760f9f0de7c15
RRR-dot43 <ruhilrohit10@gmail.com>
2025-05-07 13:33:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 api_id = "25967358"
 api_hash = "10a5a31171dfdc323efdbcf84a8fb016"
 bot_token = "7844118495:AAEPSmKwyHhgMUNNpIORPWGfatMfXf4Qi5A"
-log_channel = -1002601604234
+log_channel = -1002601829579
 force_sub_channel = -1002528475274
